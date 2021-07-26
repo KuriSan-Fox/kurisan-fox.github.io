d13050d8b70aa7fb460f1086b6ec8a31fc44eaca
KuriSan_Fox <christophershefox@gmail.com>
2021-07-27 01:04:35 +0900

    偽２ちゃんねるアップデート 0.0.1

--- assets/code/js/2ch_lie_thread_generator.js
+++ assets/code/js/2ch_lie_thread_generator.js
@@ -32,10 +32,28 @@ let thread_res_creation = 0;
 let thread_res_youbi= new Array("日","月","火","水","木","金","土");
 let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','/'];
 let thread_owner_ID = ((alphabet[(Math.floor(Math.random(0,1)*63))]) + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))]);
+let keijiban_namae = 0;
+let thread_custom_owner_name_style = 0;
+let thread_owner_custom_id = 0;
+let thread_default_name = 0;
+let thread_owner_custom_name_style = 0;
 console.log(thread_owner_ID);
 let thread_ress_count = 1;
 function create_new(){
     //check();
+    keijiban_namae = document.getElementById("keijiban_namae").value;
+    thread_owner_custom_name_style = document.getElementById("thread_owner_custom_name_style").value;
+    thread_owner_custom_id = document.getElementById("thread_owner_custom_id").value;
+    thread_default_name = document.getElementById("thread_default_name").value;
+    if(thread_owner_custom_id!=''){
+        thread_owner_ID = thread_owner_custom_id;
+    }
+    if(keijiban_namae==''){
+        keijiban_namae = '偽２ちゃんねる';
+    }
+    if(thread_default_name==''){
+        thread_default_name = '偽物を見つけた無名さん';
+    }
     success_count = 0;
     console.log('スレッド作成を開始します。。。');
     // --- スレタイ準備 --- //
@@ -72,7 +90,7 @@ function create_new(){
         success_count = success_count + 1;
         console.log('イッチの名前が空白です。');
         console.log('名前を無名に設定しています。。。');
-        thread_owner_name = '偽物を見つけた無名さん';
+        thread_owner_name = thread_default_name;
         console.log('名前を' + thread_owner_name + 'に設定しました。');
     }else{
         if((thread_owner_name == 'error') || (thread_owner_name == 'あぼーん')){
@@ -134,15 +152,11 @@ function thread_render(){
     thread_res_creation = new Date();
     console.log('スレッドID：' + get_unixtime_a);
     thread_dummy_id = (get_unixtime_a);
-    /*const element = document.querySelector('div');
-    element.remove();*/
     success_count = 4;
-    // スレッドID（仮）
-    window.location.href = ('?thread_id=' + thread_dummy_id + 'creation=true#');
     // スレッド書き込みふぉーむ
-    document.write('<div id="kakikomi" style="position:absolute;bottom:-200px;left:0px;right:0px;"><div style="text-align:center;"><a href="">リロード[Ctrl+R]</a> <a href="">新着レスの表示</a> / <small><a href="">スレッドの保存</a></small></div><hr>　⇒<hr><a href="">掲示板に戻る</a> <a href="">全部</a> <a href="">前100</a> <a href="">次100</a> <a href="">最新50</a><br><script src="../../assets/code/js/2ch_lie_thread_generator_sub.js"></script><button onclick="ress();"><a href="#kakikomi">書き込む</a></button>名前：<input type=name id="ress_name"> E-mail<small>飾り</small>：<input type=ress_email_address><div id=create_ress_desc><textarea cols=64 rows="6" id=ress_description></textarea></div><br></div>');
+    document.write('<div id="kakikomi" style="position:absolute;bottom:-250px;left:0px;right:0px;"><div style="text-align:center;"><a href="">リロード[Ctrl+R]</a> <a href="">新着レスの表示</a> / <small><a href="">スレッドの保存</a></small></div><hr>　⇒<hr><a href="">掲示板に戻る</a> <a href="">全部</a> <a href="">前100</a> <a href="">次100</a> <a href="">最新50</a><br><script src="../../assets/code/js/2ch_lie_thread_generator_sub.js"></script><button onclick="ress();"><a href="#kakikomi">書き込む</a></button>名前：<input type=name id="ress_name"> E-mail<small>実際に保存されます。実在する他人のメールアドレスは使わないでください。</small>：<input type=ress_email_address><div id=create_ress_desc><textarea cols=64 rows="6" id=ress_description></textarea></div><br><input type=checkbox id=thread_owner_mode>スレ主モード　カスタムIDを使う：<input type=text id=use_custom_id>[TIP：IDは９桁で、大文字小文字数字一部記号が使われます。空っぽにするとランダムになります。]<br>荒らしシミュレーション：<input type=checkbox id=troller>=> 有効の場合の連打送信数<input id=trollingcount type=number>（最大1000）</div>');
     // スレッドレンダー
-    document.writeln('<head><title>【偽２ちゃんねる】- '+ thread_title+ '</title></head><body style=color:black;background-color:#EFEFEF;position:relative;padding-bottom:15px;><div id=body><div id=txt_1 style="font-size:20px;padding-bottom:25px;"><a href="">偽２ちゃんねるの掟とは？</a></div><a href="">偽２ちゃんねる</a> <a href="">スマホ用</a> <a href="">■掲示板へ戻る■</a> <a href="">全部</a> <a href="">1-</a> <a href="">最新50</a><hr><div id=title style=color:red;font-size:25px;>' + thread_title + '</div><div style="position:absolute;right:0px;top:45px;">[PR]<a href="https://twitter.com/#!/KuriSan_Fox">みんな、元気～？</a>[PR]</div></body></html>');
-    document.writeln('<br>1 ：<span style="color:green;  white-space: pre-wrap;">' + thread_owner_name + '</span>：' + (thread_res_creation.getFullYear()) + '/' + (thread_res_creation.getMonth()+1) + '/' + (thread_res_creation.getDate()) + '（' + thread_res_youbi[thread_res_creation.getDay()] + '） ' + thread_res_creation.getHours() + ':' + thread_res_creation.getMinutes() + ':' + thread_res_creation.getSeconds() + '<small style="color:gray;">.' + Math.floor(thread_res_creation.getMilliseconds()*0.1) + '</small>' + ' ID: ' + thread_owner_ID);
-    document.writeln('<br>　　 ' + thread_description);
+    document.writeln('<head><title>【' + keijiban_namae + '】- '+ thread_title+ '</title></head><body style=color:black;background-color:#EFEFEF;position:relative;padding-bottom:15px;><div id=body><div id=txt_1 style="font-size:20px;padding-bottom:25px;"><a href="">' + keijiban_namae + 'の掟とは？</a></div><a href="">' + keijiban_namae + '</a> <a href="">スマホ用</a> <a href="">■掲示板へ戻る■</a> <a href="">全部</a> <a href="">1-</a> <a href="">最新50</a><hr><div id=title style=color:red;font-size:25px;>' + thread_title + '</div><div style="position:absolute;right:0px;top:45px;">[PR]<a href="https://twitter.com/#!/KuriSan_Fox">みんな、元気～？</a>[PR]</div></body></html>');
+    document.writeln('<br>1 ：<span style="color:green;' + thread_owner_custom_name_style + '">' + thread_owner_name + '</span>：' + (thread_res_creation.getFullYear()) + '/' + (thread_res_creation.getMonth()+1) + '/' + (thread_res_creation.getDate()) + '（' + thread_res_youbi[thread_res_creation.getDay()] + '） ' + thread_res_creation.getHours() + ':' + thread_res_creation.getMinutes() + ':' + thread_res_creation.getSeconds() + '<small style="color:gray;">.' + Math.floor(thread_res_creation.getMilliseconds()*0.1) + '</small>' + ' ID: ' + thread_owner_ID);
+    document.writeln('<div style="padding-left:40px;">' + thread_description + '</div>');
 }
--- assets/code/js/2ch_lie_thread_generator_sub.js
+++ assets/code/js/2ch_lie_thread_generator_sub.js
@@ -1,10 +1,70 @@
+let thread_ress_ID = 0;
+let res_user_owner_mode = ((document.getElementById("thread_owner_mode")));
+let troller_count = document.getElementById("trollingcount");
+let use_troll_mode = document.getElementById("troller");
+let trolling = 0;
 function ress() {
-    console.log('Hello World');
-    let res_name = (document.getElementById("ress_name").value);
-    let res_desc = (document.getElementById("ress_description").value);
-    let thread_ress_ID = ((alphabet[(Math.floor(Math.random(0,1)*63))]) + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))]);
-    thread_ress_count += 1;
-    let thread_ress_creation = new Date();
-    document.write('<div><br><br>' + thread_ress_count +' ：<span style="color:green;  white-space: pre-wrap;">' + res_name + '</span>：' + (thread_ress_creation.getFullYear()) + '/' + (thread_ress_creation.getMonth()+1) + '/' + (thread_ress_creation.getDate()) + '（' + thread_res_youbi[thread_ress_creation.getDay()] + '） ' + thread_ress_creation.getHours() + ':' + thread_ress_creation.getMinutes() + ':' + thread_ress_creation.getSeconds() + '<small style="color:gray;">.' + Math.floor(thread_ress_creation.getMilliseconds()*0.1) + '</small>' + ' ID: ' + thread_ress_ID);
-    document.writeln('<br>　　 ' + res_desc + '</div>');
+    trolling = 0;
+    troller_count = (document.getElementById("trollingcount").value);
+    use_troll_mode = document.getElementById("troller");
+    if(use_troll_mode.checked){
+        console.log('荒らしモードが有効です。');
+    }else{
+        console.log('荒らしモードが無効です。');
+        troller_count = 1;
+    }
+    console.log(troller_count);
+    while(trolling<troller_count){
+        trolling += 1;
+        let res_name = (document.getElementById("ress_name").value);
+        let res_desc = (document.getElementById("ress_description").value);
+        if (res_desc == '') {
+            alert('何か入力してください。');
+        } else {
+            if ((res_name.match(/agemoti/))||(res_name.match(/あげもち/))||(res_name.match(/ぼうにんげん/))||(res_name.match(/bouningen/))) {
+                alert('使ったら呪われますよ。');
+            } else {
+                res_user_owner_mode = ((document.getElementById("thread_owner_mode")));
+                if (res_user_owner_mode.checked) {
+                    res_user_owner_mode = 1;
+                } else {
+                    res_user_owner_mode = 0;
+                }
+                console.log('スレ主モード：' + res_user_owner_mode);
+                generate_random_id();
+                thread_ress_count += 1;
+                let thread_ress_creation = new Date();
+                if(res_user_owner_mode){
+                    res_name = thread_owner_name;
+                    thread_ress_ID = thread_owner_ID;
+                }
+                if (res_name == '') {
+                    res_name = thread_default_name;
+                }
+                document.write('<div><br>' + thread_ress_count +' ：<span style="color:green;  white-space: pre-wrap;">' + res_name + '</span>：' + (thread_ress_creation.getFullYear()) + '/' + (thread_ress_creation.getMonth()+1) + '/' + (thread_ress_creation.getDate()) + '（' + thread_res_youbi[thread_ress_creation.getDay()] + '） ' + thread_ress_creation.getHours() + ':' + thread_ress_creation.getMinutes() + ':' + thread_ress_creation.getSeconds() + '<small style="color:gray;">.' + Math.floor(thread_ress_creation.getMilliseconds()*0.1) + '</small>' + ' ID: ' + thread_ress_ID);
+                document.writeln('<div style="padding-left:40px;">' + res_desc + '</div></div>');
+            }
+        }
+    }
 }
+
+function generate_random_id() {
+    let thread_ress_ID_get = (document.getElementById("use_custom_id").value);
+    if (((thread_ress_ID_get == '') || (thread_ress_ID_get == 'undefined'))) {
+        thread_ress_ID = ((alphabet[(Math.floor(Math.random(0,1)*63))]) + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))] + alphabet[(Math.floor(Math.random(0,1)*63))]);
+    } else {
+        thread_ress_ID = thread_ress_ID_get;
+    }
+    if (res_user_owner_mode) {
+        thread_ress_ID = thread_owner_ID;
+    }
+}
+
+/*
+こういうことできるよーってな感じのスレです。<br>
+書き込んでも保存されないのでリロードすると消えます。<br>
+実際に使った後のhtmlとstyleをコピペして新しいhtmlに貼り付けてます。<br>
+そういう使い方もいいよ。<br>
+誹謗中傷とかデマ流しには使わないでね。<br>
+<a href="before.html">書き込む前に読んでね</a>は絶対読んでね。
+*/
